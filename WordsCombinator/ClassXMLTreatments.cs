e7637e7344eb59e948c898477663b9ac27904ae6
ADRIEN MARINI <adrien.marini@viacesi.fr>
2019-05-06 20:26:35 +0200

    ajout d'un fichier xml pour sauvegarderles précédents mots entrés + création d'une classe cs pour gérer le fichier xml

---
+++ WordsCombinator/ClassXMLTreatments.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by Adrien Marini - Copyright 2019
+ * 
+ * Words Combinations is a program created to make words combinations.
+ * 
+ * LICENCE :
+ * 
+ * This file is part of Words Combinations.
+
+    Words Combinations is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Words Combinations is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Words Combinations.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WordsCombinator
+{
+    public static class ClassXmlTreatments
+    {
+        private const string configDirPath = @"..\debug\";
+
+
+    }
+}
